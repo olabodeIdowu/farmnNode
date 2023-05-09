7a058e0df14bc3ed68179e9cc3ec836e3ef54e38
olabodeIdowu <120285585+olabodeIdowu@users.noreply.github.com>
2023-05-09 14:19:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
+// ///////////////////////
+// FILES
+// const fs = require('fs');
+// blocking
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textIn)
+// const textOut = `blocking & non-blocking asynchronous nature: ${textIn} \n${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log('file written')
+
+// // non-blocking, asynchrous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1)=>{
+// fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+// fs.readFile('./txt/append.txt', 'utf-8', (err, data3)=>{
+// fs.writeFile('./txt/final.txt', `${data2}\n ${data3}`, 'utf-8', err=>{
+//   console.log('your file have been written');
+// })
+// })
+// });
+// })
+// console.log('will read file');
+
+// ///////////////////////
+// SERVER
+
+// console.log(fs, http, url)
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+// console.log(tempCard, tempOverview, tempProduct)
+const data = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+);
+const slugs = data.map(el=>slugify(el.productName, {lower: true}))
+console.log(slugs)
+// creating server
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const cardsHtml = data.map((el) => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/, cardsHtml);
+    res.end(output);
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = data[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  } else {
+    res.writeHead(404, { 'Content-type': 'text/html' });
+    res.end('<h1>page not found🙁<h1>');
+  }
+});
+
+// listening on port 8000 localhost 127.0.0.1
+server.listen(8000, '127.0.0.1', () => {
+  console.log(
+    'listening to incoming request on localhost(computer) 127.0.0.1, port 8000 😁'
+  );
+});
